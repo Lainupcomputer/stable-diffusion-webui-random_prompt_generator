774a4d0def768762898596232ab1a4660d557450
Sandro <34354689+Lainupcomputer@users.noreply.github.com>
2023-01-11 03:26:25 +0100

    added sfw / nsfw mode

--- scripts/random_prompt_generator.py
+++ scripts/random_prompt_generator.py
@@ -16,6 +16,8 @@ class Swap:
         self.prompt_output = ""
         self.file_list = []
         self.choose_prompts = []
+        # nsfw mode
+        self.mode = True
 
     @staticmethod
     def load_from_fs(filename):
@@ -43,7 +45,13 @@ class Swap:
             self.prompt_output = self.prompt_output + line + ","
 
     def get_folder_prompts(self):
-        for file_path in glob.glob(self.script_path + self.prompt_path + "*.txt"):
+        # if nsfw
+        if self.mode:
+            p = self.prompt_path + "nsfw/"
+        else:
+            p = self.prompt_path + "sfw/"
+
+        for file_path in glob.glob(self.script_path + p + "*.txt"):
             self.file_list.append(self.load_from_fs(file_path))
 
     def choose_prompt(self):
@@ -55,6 +63,12 @@ class Swap:
             self.prompt_output = self.prompt_output + prompt + ","
 
         self.prompt_output = self.prompt_output[:-1]
+
+    def switch_mode(self):
+        if self.mode:
+            self.mode = False
+        else:
+            self.mode = True
 
     def run(self):
         self.reset()
@@ -71,6 +85,14 @@ def add_to_prompt():
     return swap.prompt_output
 
 
+def switch_mode():
+    swap.switch_mode()
+    if swap.mode:
+        return "On"
+    else:
+        return "Off"
+
+
 def on_ui_tabs():
     txt2img_prompt = modules.ui.txt2img_paste_fields[0][0]
 
@@ -78,26 +100,26 @@ def on_ui_tabs():
         with gr.Tabs():
             with gr.TabItem("Generator"):
                 with gr.Column():
-                    gen_btn = gr.Button(value="Generate", elem_id="generate_button")
+                    gen_btn = gr.Button(value="Generate", elem_id="gen_btn")
                 with gr.Column(visible=False) as results_col:
                     results = gr.Text(label="Results", elem_id="Results_textBox", interactive=False)
                 with gr.Column():
-                    warning = gr.HTML(value="Send the first generated prompt to:", visible=False)
-                    with gr.Row():
-                        send_to_txt2img = gr.Button('Send to txt2img', visible=False)
+                    send_to_txt2img = gr.Button('Send to txt2img', visible=False)
             with gr.TabItem("Configuration"):
-                gr.Markdown("this is a demo")
+                with gr.Row():
+                    mode = gr.Text(label="NSFW Mode", elem_id="mode_textBox", interactive=False)
+                    sw_btn = gr.Button(value="Switch SFW / NSFW", elem_id="sw_btn")
 
         def generate_prompts():
             swap.run()
             return {results: swap.prompt_output,
                     send_to_txt2img: gr.update(visible=True),
-                    results_col: gr.update(visible=True),
-                    warning: gr.update(visible=True)
+                    results_col: gr.update(visible=True)
                     }
 
         # events
-        gen_btn.click(fn=generate_prompts, outputs=[results, send_to_txt2img, results_col, warning])
+        sw_btn.click(fn=switch_mode, outputs=[mode])
+        gen_btn.click(fn=generate_prompts, outputs=[results, send_to_txt2img, results_col])
         send_to_txt2img.click(add_to_prompt, outputs=[txt2img_prompt])
         send_to_txt2img.click(None, _js='switch_to_txt2img', inputs=None, outputs=None)
     return (prompt_generator, "Random-Prompt-Gen", "Random-Prompt-Gen"),
