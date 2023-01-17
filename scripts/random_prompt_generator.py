87befa326123cd5cec9768a022839cc3435ff2b1
Sandro <34354689+Lainupcomputer@users.noreply.github.com>
2023-01-17 09:32:53 +0100

    added ez_storage checking / import

--- scripts/random_prompt_generator.py
+++ scripts/random_prompt_generator.py
@@ -4,6 +4,13 @@ from modules import script_callbacks
 import glob
 import random
 
+try:
+    from ez_storage.ez_storage import Ez_Storage
+except ModuleNotFoundError:
+    print("ez_storage module is missing (pip install ez-storage)")
+
+default = Ez_Storage("./extensions/stable-diffusion-webui-random_prompt_generator/Prompts/default.ezs")
+
 
 class Swap:
 
